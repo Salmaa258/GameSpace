8ee238fac1730bfa2243c9fc599e890d4d880159
Salma Picazo Tió <picazo.tio.salma@alumnat.copernic.cat>
2023-01-16 08:30:49 +0100

    Tasca 5 Creación pantalla Login

---
+++ app/src/main/java/cat/copernic/gamespace/Login.kt
@@ -0,0 +1,12 @@
+package cat.copernic.gamespace.Activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cat.copernic.gamespace.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
